57aa1e6ffba6e3f2bce278bd4ec05cdfdd83d37b
Evan Burton <butonevan1@gmaail.com>
2021-11-08 09:10:54 -0700

    added members array and jpeg

---
+++ models/member.js
@@ -0,0 +1,39 @@
+const members = [{
+    
+    name: 'Evan Burton',
+    profilePic: './assets/images/evanpic.jpg',
+    title: 'Back-End',
+    bio: 'Fill this in at some point',
+    github: 'https://github.com/Rankorrdagod'
+
+},
+{
+    
+    name: '',
+    profilePic: '',
+    title: '',
+    bio: '',
+    github: ''
+
+},
+{
+    
+    name: '',
+    profilePic: '',
+    title: '',
+    bio: '',
+    github: ''
+
+},
+{
+    
+    name: '',
+    profilePic: '',
+    title: '',
+    bio: '',
+    github: ''
+
+}
+
+];
+module.exports = members
