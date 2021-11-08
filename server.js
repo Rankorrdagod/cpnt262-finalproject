703ce32b1311da55d8b1a0a1becf618c0d129b38
Filora T <filorat@outlook.com>
2021-11-07 20:43:38 -0700

    added new asset, updated html

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-const express = require('express')
-const mongoose = require('mongoose');
-const dotenv = require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
 
-mongoose.connect(
-  process.env.MONGODB_URL,
-  { useUnifiedTopology: true, useNewUrlParser: true },
-  )
-  .then(function(){
-    console.log('Connected to DB...')
-  })
-  .catch(function(err){
-    console.log(err)
-  });
+mongoose
+	.connect(process.env.MONGODB_URL, {
+		useUnifiedTopology: true,
+		useNewUrlParser: true,
+	})
+	.then(function () {
+		console.log("Connected to DB...");
+	})
+	.catch(function (err) {
+		console.log(err);
+	});
