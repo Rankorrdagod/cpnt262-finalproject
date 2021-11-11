b09de35fe5c32d16baa06069dff636494a1866d0
Evan Burton <butonevan1@gmaail.com>
2021-11-11 12:04:35 -0700

    update to backend

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use((req, res) => {
 });
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, function(){
   console.log(`Listening on port ${PORT}`);
