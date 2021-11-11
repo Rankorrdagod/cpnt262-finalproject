30e866cc1255168e187827b5d64e11f28a70e4e0
Evan Burton <90275485+Rankorrdagod@users.noreply.github.com>
2021-11-11 09:16:03 -0700

    api routes
    gallery routes

--- routes/api.js
+++ routes/api.js
@@ -1,134 +1,51 @@
-const gallery = [
-	{
-		id: 1,
-		title: Kitchen,
-		description: 
-		width: 
-		height: 
-		pathURL:
-		linkURL: 
-		credit: 
-		creditURL: 
-	},
-	{
-		id: 2,
-		title: 
-		description: 
-		width: 
-		height: 
-		pathURL:
-		linkURL: 
-		credit: 
-		creditURL: 
-	},
-	{
-		id: 3,
-		title: 
-		description: 
-		width: 
-		height: 
-		pathURL:
-		linkURL: 
-		credit: 
-		creditURL: 
-	},
-	{
-		id: 4,
-		title: 
-		description: 
-		width: 
-		height: 
-		pathURL:
-		linkURL: 
-		credit: 
-		creditURL: 
-	},
-	{
-		id: 5,
-		title: 
-		description: 
-		width: 
-		height: 
-		pathURL:
-		linkURL: 
-		credit: 
-		creditURL: 
-	},
-	{
-		id: 6,
-		title: 
-		description: 
-		width: 
-		height: 
-		pathURL:
-		linkURL: 
-		credit: 
-		creditURL: 
-	},
-	{
-		id: 7,
-		title: 
-		description: 
-		width: 
-		height: 
-		pathURL:
-		linkURL: 
-		credit: 
-		creditURL: 
-	},
-	{
-		id: 8,
-		title: 
-		description: 
-		width: 
-		height: 
-		pathURL:
-		linkURL: 
-		credit: 
-		creditURL: 
-	},
-	{
-		id: 9,
-		title: 
-		description: 
-		width: 
-		height: 
-		pathURL:
-		linkURL: 
-		credit: 
-		creditURL: 
-	},
-	{
-		id: 10,
-		title: 
-		description: 
-		width: 
-		height: 
-		pathURL:
-		linkURL: 
-		credit: 
-		creditURL: 
-	},
-	{
-		id: 11,
-		title: 
-		description: 
-		width: 
-		height: 
-		pathURL:
-		linkURL: 
-		credit: 
-		creditURL: 
-	},
-	{
-		id: 12,
-		title: 
-		description: 
-		width: 
-		height: 
-		pathURL:
-		linkURL: 
-		credit: 
-		creditURL: 
-	},
-]
+const express = require('express')
+const router = express.Router()
+
+const Gallery = require('../models/gallery') 
+const localLocation = require('../models/seeds/gallery')
+
+
+
+
+
+
+router.get('/gallery', async (req, res) => {
+    try {
+      let data = await Location.find()
+  
+      
+      if (data.length === 0) {
+        data = localGallery
+      }
+     else { 
+        res.send(data) 
+      }
+    } catch (err) {
+        console.log(err) 
+        res.send({ error: 'Gallery Not Found' }) // send JSON 404 error
+      }
+    })
+
+    router.get('/gallery/:id', async (req, res) => {
+        try {
+       
+          let data = await Gallery.findOne({ id: req.params.id })
+      
+          
+          if (!data) {
+            data = localGallery.find(Location => Number(req.params.id) === Location.id) 
+          }
+      
+          if (data) { 
+            res.send(data) 
+          } else { 
+            res.send({ error: 'Gallery Doesnt Exist' }) // send JSON 404 error
+          }
+      
+        } catch (err) {
+          console.log(err) 
+          res.send({ error: '404 Not Found' }) // send JSON 404 error
+        }
+      })
+  
+  module.exports = router
