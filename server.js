eae288a29cbd24ea44b4493d453c8ba6e2b91e81
Filora T <filorat@outlook.com>
2021-11-11 20:08:22 -0700

    fixed issues with server.js

--- server.js
+++ server.js
@@ -1,37 +1,32 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const api = require("./routes/api");
 
+async function main() {
+	await mongoose.connect(process.env.MONGO_URI);
+	const app = express();
 
-const dotenv = require('dotenv').config();
-const express = require('express')
+	app.use(express.static("public"));
+	app.use("/api", api);
 
-const app = express();
+	// Handle 404 errors with middleware
+	app.use((req, res) => {
+		try {
+			if (req.url.startsWith("/api")) {
+				res.status(404).send({ error: "404 Not Found" });
+			} else {
+				res.status(404).redirect("/404.html");
+			}
+		} catch {
+			res.status(404).send({ error: "404 Not Found" });
+		}
+	});
 
-app.use(express.static('public'))
+	const PORT = process.env.PORT || 3001;
 
-
-
-const api = require('./routes/api')
-app.use('/api', api)
-
-const mongoose = require('./models/db')
-
-// Handle 404 errors with middleware
-app.use((req, res) => {
-  try {
-
-    if (req.url.startsWith('/api')) {
-      res.status(404).send({ error: '404 Not Found' })
-    } else {
-     
-      res.status(404).redirect('/404.html') 
-    }
-  } catch {
-    res.status(404).send({ error: '404 Not Found' }) 
-  }
-});
-
-
-const PORT = process.env.PORT || 3001;
-
-app.listen(PORT, function(){
-  console.log(`Listening on port ${PORT}`);
-});
+	app.listen(PORT, function () {
+		console.log(`Listening on port ${PORT}`);
+	});
+}
+main();
