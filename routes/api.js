6d4881d3f1d4c2cc132653d2c8ef6f395e049e16
Filora T <filorat@outlook.com>
2021-11-11 20:26:14 -0700

    updated subcriber.js and api.js

--- routes/api.js
+++ routes/api.js
@@ -1,51 +1,45 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const Gallery = require('../models/gallery') 
-const localGallery = require('../models/seeds/gallery')
-const members = require('../models/member')
-const subscribers = require('../models/seeds/subscriber')
+const Gallery = require("../models/gallery");
+const localGallery = require("../models/seeds/gallery");
+const members = require("../models/member");
+const subscribers = require("../models/seeds/seed-subscribers");
 
+router.get("/gallery", async (req, res) => {
+	try {
+		let data = await Gallery.find();
 
+		if (data.length === 0) {
+			data = localGallery;
+		} else {
+			res.send(data);
+		}
+	} catch (err) {
+		console.log(err);
+		res.send({ error: "Gallery Not Found" }); // send JSON 404 error
+	}
+});
 
+router.get("/gallery/:id", async (req, res) => {
+	try {
+		let data = await Gallery.findOne({ id: req.params.id });
 
-router.get('/gallery', async (req, res) => {
-    try {
-      let data = await Gallery.find()
-  
-      
-      if (data.length === 0) {
-        data = localGallery
-      }
-     else { 
-        res.send(data) 
-      }
-    } catch (err) {
-        console.log(err) 
-        res.send({ error: 'Gallery Not Found' }) // send JSON 404 error
-      }
-    })
+		if (!data) {
+			data = localGallery.find(
+				(Gallery) => Number(req.params.id) === Gallery.id
+			);
+		}
 
-    router.get('/gallery/:id', async (req, res) => {
-        try {
-       
-          let data = await Gallery.findOne({ id: req.params.id })
-      
-          
-          if (!data) {
-            data = localGallery.find(Gallery => Number(req.params.id) === Gallery.id) 
-          }
-      
-          if (data) { 
-            res.send(data) 
-          } else { 
-            res.send({ error: 'Gallery Doesnt Exist' }) // send JSON 404 error
-          }
-      
-        } catch (err) {
-          console.log(err) 
-          res.send({ error: '404 Not Found' }) // send JSON 404 error
-        }
-      })
-  
-  module.exports = router
+		if (data) {
+			res.send(data);
+		} else {
+			res.send({ error: "Gallery Doesnt Exist" }); // send JSON 404 error
+		}
+	} catch (err) {
+		console.log(err);
+		res.send({ error: "404 Not Found" }); // send JSON 404 error
+	}
+});
+
+module.exports = router;
