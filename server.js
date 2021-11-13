197b26b7849f6ceeb07e2063c4044af46aae46b6
Evan Burton <butonevan1@gmaail.com>
2021-11-13 12:35:13 -0700

    merge

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const api = require("./routes/api");
 
 async function main() {
-	await mongoose.connect(process.env.MONGO_URI);
+	await mongoose.connect(process.env.MONGO_URL);
 	const app = express();
 
 	app.use(express.static("public"));
