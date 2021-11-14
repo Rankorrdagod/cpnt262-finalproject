f88d3e0f401e17a096f030869790248437d9b69b
Evan Burton <butonevan1@gmaail.com>
2021-11-14 13:39:50 -0700

    update

--- models/seeds/subscribers.js
+++ models/seeds/subscribers.js
@@ -1,21 +1,21 @@
 module.exports = [
   {
-    "id":1,
+    
     "name":"Isha Simran",
     "email":"",
   },
   {
-    "id":2,
+    
     "name":"Devon Lipsius",
     "email":"",
   },
   {
-    "id":3,
+    
     "name":"Filora Turdi",
     "email":"",
   },
   {
-    "id":4,
+    
     "name":"Evan Burton",
     "email":"burtonevan1@gmai.com",
   }
